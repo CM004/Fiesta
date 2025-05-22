fa39b81abe134a700a29280e165656fea8e3a606
Chandramohan <Senchandramohan2018@gmail.com>
2025-05-22 12:24:48 +0530

    Initial Commit

---
+++ Fiesta/FiestaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiestaApp.swift
+//  Fiesta
+//
+//  Created by Chandramohan on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FiestaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
